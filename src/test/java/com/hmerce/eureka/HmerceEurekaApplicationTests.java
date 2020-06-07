c184f3718b65e255454a1f20ba539080d4751a47
dragan <dragan.velkovski@codeit.mk>
2020-06-07 17:08:19 +0200

    project create with spring initializr and eureka server setup

---
+++ src/test/java/com/hmerce/eureka/HmerceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmerce.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmerceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
